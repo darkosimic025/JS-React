c3dda0d0b9eab0880445c9d7efd1500cc529a0ac
darkosimic025 <darkosimic025@gmail.com>
2022-02-02 18:18:16 +0100

    Push Task 8

--- 28_CHAT_PROJECT/app.js
+++ 28_CHAT_PROJECT/app.js
@@ -3,19 +3,33 @@ import ChatUi from "./ui.js"
 
 let chat1 = new Chatroom("general", "darko");
 
-chat1
-  .addChat("Neka poruka")
-  .then(() => {
-    console.log("Uspesno dodat cet");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-console.log(chat1.username);
+// chat1
+//   .addChat("Neka poruka")
+//   .then(() => {
+//     console.log("Uspesno dodat cet");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// console.log(chat1.username);
 
-chat1.getChats((d) => {
-  console.log(d);
-});
+
 
 let lista = document.querySelector('ul');
 let chatUi = new ChatUi(lista);
+
+chat1.getChats((d) => {
+    chatUi.templateLi(d)
+});
+  
+let inputMessage = document.querySelector('#inputMessage');
+let buttonSend = document.querySelector('#buttonSend');
+
+buttonSend.addEventListener('click', (e) => {
+    e.preventDefault();
+    let inputMessageValue = inputMessage.value;
+    chat1.addChat(inputMessageValue);
+    chat1.getChats((data) => {
+        chatUi.templateLi(data);
+    })
+})
--- 28_CHAT_PROJECT/ui.js
+++ 28_CHAT_PROJECT/ui.js
@@ -9,12 +9,34 @@ class ChatUi {
 
   get element() {
     return this._element;
-  }
+    }
+    
+    dateMethod(date) {
+        let d = date.getDay();
+        let m = date.getMonth()+1;
+        let y = date.getFullYear();
+        let h = date.getHours();
+        let min = date.getMinutes();
+
+        // Jelena resenje:
+        // d = String(d).padStart(2, "0");
+        // m = String(m).padStart(2,"0");
+        // h = String(h).padStart(2,"0");
+        // min = String(min).padStart(2, "0");
+
+        // Darko resenje:
+
+        return `${d < 9 ? "0" + d : d}.${m < 9 ? "0" + m : m}.${y}.   ${h < 9 ? "0" + h : h}:${min < 9 ? "0" + min : min}`;
+    }
 
   templateLi(object) {
-      let li = `<li>${object.username}: ${object.message} (${object.created_at})<li>`;
-      this.element.innerHTML += li;
+    let dateFormat = object.created_at.toDate();
+
+    let li = `<li>${object.username}: ${
+      object.message
+    } (${this.dateMethod(dateFormat)})</li>`;
+    this.element.innerHTML += li;
   }
 }
 
-export default ChatUi
+export default ChatUi;
